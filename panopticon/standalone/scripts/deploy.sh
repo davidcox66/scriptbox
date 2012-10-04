5b461d32ad00bf77f2acfa11833685d56c3b6705
coxda <david.cox@ihg.com>
2012-10-04 16:01:49 -0400

    Tweaks to scripts

--- panopticon/standalone/scripts/deploy.sh
+++ panopticon/standalone/scripts/deploy.sh
@@ -5,6 +5,6 @@ shift 1
 set -x
 for host in $* ; do
 	ssh $host mkdir -p $remote_dir
-	scp $dir/monitor $dir/monitorcli $dir/../target/panopticon.sa-*-standalone-distro.jar $host:$remote_dir
-	# scp $dir/monitor $dir/monitorcli $host:$remote_dir
+	# scp $dir/monitor $dir/monitorcli $dir/monitor.server $dir/../target/panopticon.sa-*-standalone-distro.jar $host:$remote_dir
+	scp $dir/monitor $dir/monitorcli $dir/monitor.server $host:$remote_dir
 done
